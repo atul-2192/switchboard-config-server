67ef157e4e29ae7b94b5e415f3e9e7689b487eaa
Vaishali Chauhan <94157880+vaishali515@users.noreply.github.com>
2025-12-28 23:24:17 +0530

    Initial Commit

---
+++ src/test/java/com/example/Switchboard/config/server/SwitchboardConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Switchboard.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitchboardConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
